15fc7f94a77245d755889b20f9d9565c3d026b0f
Advika <advikakaushik.16@gmail.com>
2026-09-02 01:32:45 +0530

    Add RISK_CONFIG for Mapbox public token

---
+++ config.js
@@ -0,0 +1,5 @@
+// Keep this file in the same folder as index.html.
+// A public token is optional. Leave blank to use no-key demo search/routing fallbacks.
+window.RISK_CONFIG = {
+  MAPBOX_PUBLIC_TOKEN: ""
+};
