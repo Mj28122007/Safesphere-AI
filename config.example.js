0ac6612e309dafaf90d518fa836958fcdf1e72c1
Advika <advikakaushik.16@gmail.com>
2026-09-02 01:35:00 +0530

    Delete config.example.js

--- config.example.js
+++
@@ -1,5 +0,0 @@
-// Keep this file in the same folder as index.html.
-// A public token is optional. Leave blank to use no-key demo search/routing fallbacks.
-window.RISK_CONFIG = {
-  MAPBOX_PUBLIC_TOKEN: ""
-};
